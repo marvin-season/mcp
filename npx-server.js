911b7b212f5210eae9abcb9f0a1e1c97d16dacd3
marvin <sea-son@foxmail.com>
2025-04-16 20:28:53 +0800

    feat: mcp init

---
+++ npx-server.js
@@ -0,0 +1,43 @@
+#!/usr/bin/env node
+
+import { McpServer } from "@modelcontextprotocol/sdk/server/mcp.js";
+import { z } from "zod";
+import { StdioServerTransport } from "@modelcontextprotocol/sdk/server/stdio.js";
+const server = new McpServer({
+  name: "Echo",
+  version: "1.0.0"
+});
+
+
+// Add an addition tool
+server.tool("add",
+  { a: z.number(), b: z.number() },
+  async ({ a, b }) => ({
+    content: [{ type: "text", text: String(a + b) }]
+  })
+);
+
+server.tool("saymyname",
+  {},
+  async ({ }) => ({
+    content: [{ type: "text", text: "海森堡！" + Date.now().toLocaleString() }]
+  })
+);
+
+server.prompt(
+  "echo",
+  { message: z.string() },
+  ({ message }) => ({
+    messages: [{
+      role: "user",
+      content: {
+        type: "text",
+        text: `- Please process this message: ${message}`
+      }
+    }]
+  })
+);
+
+// Start receiving messages on stdin and sending messages on stdout
+const transport = new StdioServerTransport();
+await server.connect(transport);
